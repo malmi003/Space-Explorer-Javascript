0e6df90b1714b229af81c94dcb29cd531039bdab
Amanda Malmin <amandamalmin@gmail.com>
2018-06-15 21:26:33 -0500

    set up basic CSS styles and js psuedocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+/*
+
+- * graphic makes it to moon on win and then displays something clever, maybe change out planets so go from earth to moon, mars, jupiter, uranus, 
+    - loss sends them back to earth prematurely because of various excuses
+- * onload, generate 1 random number between 19-120 & display on page, make the distance between ship and moon that many spaces
+- * generate 4 more random numbers between 1-12 and assign to icons1-4
+- * when player clicks an icon, that number is added to their score
+- * if the number they click is <= the targetNumber, keep guessing and adding that number to their score
+    - move space ship towards moon same number of space they guessed
+- * if their score exceeds the targetNumber they lose
+    - add to loss count, reset the numbers, a meteor shower made them start mission over
+- * if their score == target number, they win, add to win count, reset game
+
+
+
+
+*/
